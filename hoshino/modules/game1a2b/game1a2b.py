21709427aada0cdfb7db46cf2133939453633b36
KuroYkksk <liangjiecheng123@gmail.com>
2021-09-27 22:31:20 +0800

    Update game1a2b.py

--- hoshino/modules/game1a2b/game1a2b.py
+++ hoshino/modules/game1a2b/game1a2b.py
@@ -1,7 +1,22 @@
+import re
+
 from hoshino import Service
+from hoshino.typing import CQEvent
 
 sv = Service('1a2b')
 
-@sv.on_rex(r'^1[Aa]2[Bb]$')
+start_1a2b = re.compile(r'^1[Aa]2[Bb]$')
+guess_1a2b = re.compile(r'^[0-9]{4}')
+is_1a2b_start = False
+
+@sv.on_rex(r'^1[Aa]2[Bb]$|^[0-9]{4}')
 async def hello(bot, ev):
-    await bot.send(ev,'echo: 1a2b test')
+    start = start_1a2b.match(ev.message)
+    guess = guess_1a2b.match(ev.message)
+    if start:
+        await bot.send(ev,'match 1a2b')
+    if guess:
+        await bot.send(ev,'match number')
+
+    # if is_1a2b_start:
+    #     await bot.send(ev,'echo: 1a2b test')
