b8015ccbe8cb9af2a0ea0d75bf21fd0dd2fcc337
KuroYkksk <liangjiecheng123@gmail.com>
2021-09-28 22:19:58 +0800

    Update game1a2b.py

--- hoshino/modules/game1a2b/game1a2b.py
+++ hoshino/modules/game1a2b/game1a2b.py
@@ -7,24 +7,32 @@ from hoshino.typing import CQEvent
 sv = Service('1a2b')
 
 start_1a2b = re.compile(r'^1[Aa]2[Bb]$')
-guess_1a2b = re.compile(r'^[0-9]{4}')
+guess_1a2b = re.compile(r'^[0-9][0-9][0-9][0-9]$')
 is_1a2b_start = False
 
 # TODO: answer generator
 def ans_gener():
-    answer = random.sample(range, 4)
+    answer = random.sample(range(10), 4)
     return answer
 
-
+# TODO: repeated judge
+def have_repeat(guess):
+    hash_set = dict()
+    guess_list = list(guess)
+    for i in range(4):
+        if guess_list[i] in hash_set:
+            return True
+        else:
+            hash_set[guess_list[i]] = None
+    return False
 
 # TODO: game judge
 def hint(answer, guess):
-    correct = sum([answer[i] == guess[i] for i in range(4)])
-    misplaced = sum([i in answer for i in guess]) - correct
-    if correct == 4:
-        return True
-    else:
-        return correct, misplaced
+    answer_list = list(answer)
+    guess_list = list(guess)
+    correct = sum([answer_list[i] == guess_list[i] for i in range(4)])
+    misplaced = sum([i in guess_list for i in guess_list]) - correct
+    return correct, misplaced
 
 
 @sv.on_rex(r'^1[Aa]2[Bb]$|^[0-9][0-9][0-9][0-9]$')
@@ -36,17 +44,17 @@ async def hello(bot, ev):
         if is_1a2b_start:
             await bot.send(ev, '1A2B已经开始，请等待游戏结束')
         else:
+            answer = ans_gener()
             is_1a2b_start = True
             await bot.send(ev, '1A2B游戏开始啦！发送你要猜的四位数字，开始游戏吧~')
     elif guess:
         if not is_1a2b_start:
             return
-        # TODO: 重复判断
-        # if 重复
-            # return
+        if have_repeat(guess):
+            return
         # TODO: game judge
         if guess == answer:
-            await bot.send(ev, '恭喜_____中了正确答案：5862！')
+            await bot.send(ev, '恭喜玩家猜中了正确答案！')
             is_1a2b_start = False
         else:
             await bot.send(ev, '_A_B')
